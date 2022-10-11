27ca1d6b0a904aa726518da0293d3748abf04c71
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-11 14:56:19 +0100

    add  components: NFTCard,index,HomeHeader,FocusedStatusBar

---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FocusedStatusBar = () => {
+  return (
+    <View>
+      <Text>hi, Amine</Text>
+    </View>
+  )
+} 
+
+export default FocusedStatusBar
---
+++ components/index.js
@@ -0,0 +1,9 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+
+export{
+    HomeHeader,
+    FocusedStatusBar,
+    NFTCard
+}
