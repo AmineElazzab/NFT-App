0c7fb0770befd1cb1230f7f77637f88e3db5e264
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-12 23:33:21 +0100

    update components/index

--- components/index.js
+++ components/index.js
@@ -1,9 +1,12 @@
 import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
+import {CircleButton, RectButton} from "./Button";
 
 export{
     HomeHeader,
     FocusedStatusBar,
-    NFTCard
+    NFTCard,
+    CircleButton,
+    RectButton
 }
