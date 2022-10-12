18cff7cee629203643ba341603af13518d018a8c
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-12 23:31:05 +0100

    update FocusedStatusBar

--- components/FocusedStatusBar.js
+++ components/FocusedStatusBar.js
@@ -1,12 +1,9 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StatusBar } from 'react-native'
+import { useIsFocused } from '@react-navigation/core'
 
-const FocusedStatusBar = () => {
-  return (
-    <View>
-      <Text>hi, Amine</Text>
-    </View>
-  )
-} 
+const FocusedStatusBar = (props) => {
+  const isFocused = useIsFocused()
+  return isFocused ? <StatusBar animated={true} {...props} /> : null
 
+}
 export default FocusedStatusBar
