f9bf22162078b4b7b44805dd701af7acb0fd5c07
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-12 23:34:02 +0100

    update screens/Home

--- screens/Home.js
+++ screens/Home.js
@@ -7,10 +7,36 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Home = () => {
   return (
-    <SafeAreaView style={{flex:1}}>
-      <FocusedStatusBar  backgroundColor={COLORS.black} />
-      </SafeAreaView>
-      
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar backgroundColor={COLORS.gray} />
+
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 1 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <NFTCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+
+          />
+        </View>
+        <View style={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          zIndex: -1,
+        }
+        }>
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+        </View>
+      </View>
+
+    </SafeAreaView>
+
   )
 }
 
