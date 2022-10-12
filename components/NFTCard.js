b3f6199e9ecbb0cd602b0a97ab90632c404c27d9
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-12 23:32:20 +0100

    update NFTCard

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,9 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { COLORS, SIZES, SHADOWS, assets } from '../constants';
+import { CircleButton, RectButton } from './Button';
 
-const NFTCard = () => {
+const NFTCard = ({ data }) => {
+  const navigation = useNavigation();
+
   return (
-    <View>
+    <View style={{
+      backgroundColor: COLORS.white,
+      borderRadius: SIZES.font,
+      marginBottom: SIZES.extraLarge,
+      margin: SIZES.base,
+      ...SHADOWS.dark,
+    }}>
+      <View style={{ width: "100%", height: 250 }}>
+        <Image
+          source={data.image}
+          resizeMode="cover"
+          style={{
+            width: "100%",
+            height: "100%",
+            borderRadius: SIZES.font,
+          }}
+        />
+
+        <CircleButton imgURL={assets.heart} right={10} top={10} />
+      </View>
       <Text>NFTCard</Text>
     </View>
   )
